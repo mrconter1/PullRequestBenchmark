c55eeefa60e97cc79397502b46985e910a4b1e9e
Rasmus Lindahl <mrconter1@gmail.com>
2024-03-12 21:58:38 +0100

    Delete pull_request_benchmark/evaluation_sample.py

--- pull_request_benchmark/evaluation_sample.py
+++
@@ -1,8 +0,0 @@
-class EvaluationSample:
-    def __init__(self, pr_url):
-        self.pull_request = PullRequest(pr_url)
-        self.repository = Repository(self.pull_request.repo_url)
-        
-    def get_data_point_context_size(self):
-        # To be implemented
-        pass
