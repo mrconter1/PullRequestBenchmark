38457a773294787d7a17aaf9051833456688ff8d
Rasmus Lindahl <rasmus.lindahl@se.abb.com>
2024-03-17 22:33:07 +0100

    Add code to extract the pr url from the html

--- pull_request_benchmark/models/evaluation_sample.py
+++ pull_request_benchmark/models/evaluation_sample.py
@@ -1,6 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
 class EvaluationSample:
     def __init__(self, pr_url):
-        self.t = 1
+        self.pr_url = pr_url
+        self.pr_title = self.extract_pr_title(pr_url)
+        print(self.pr_title)
+        
+    def extract_pr_title(self, url):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        title_tag = soup.find('bdi', class_='js-issue-title markdown-title')
+        return title_tag.text.strip() if title_tag else 'Title not found'
         
     def get_pr_type():
         return "lol"
