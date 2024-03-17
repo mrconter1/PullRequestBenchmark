1760aa1caa291d29d3348708039060b1ee2e6249
Rasmus Lindahl <rasmus.lindahl@se.abb.com>
2024-03-17 23:40:04 +0100

    Add so that git log patch command for history up until just before PR can be fetched

--- pull_request_benchmark/models/evaluation_sample.py
+++ pull_request_benchmark/models/evaluation_sample.py
@@ -12,6 +12,12 @@ class EvaluationSample:
         self.pr_description = self.pr_info.get('body', '')
         self.pr_diff = self.fetch_pr_patch()
         
+        self.base_sha = self.pr_info.get('base', {}).get('sha', '')
+        self.git_log_patch_command = self.generate_git_log_patch_command()
+        
+        print(self.git_log_patch_command)
+        input()
+        
     def fetch_pr_info(self):
         response = requests.get(self.api_url, headers={'Accept': 'application/vnd.github.v3+json'})
         if response.ok:
@@ -23,3 +29,10 @@ class EvaluationSample:
         if response.ok:
             return response.text
         return {}
+    
+    def generate_git_log_patch_command(self):
+        # Using the base commit SHA to generate the git log command
+        if self.base_sha:
+            return f"git log --patch {self.base_sha}^"
+        else:
+            return "Base commit SHA not found"
