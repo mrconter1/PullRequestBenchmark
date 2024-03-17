af356deb45fd390ef4bc43a954746af0a84f2551
Rasmus Lindahl <rasmus.lindahl@se.abb.com>
2024-03-17 23:30:36 +0100

    Extract pr diff as well

--- pull_request_benchmark/models/evaluation_sample.py
+++ pull_request_benchmark/models/evaluation_sample.py
@@ -4,18 +4,22 @@ class EvaluationSample:
     
     def __init__(self, entry):
         
-        self.pr_info = self.fetch_pr_info(entry)
+        self.api_url = f"https://api.github.com/repos/{entry['Owner']}/{entry['Repository']}/pulls/{entry['PullRequestId']}"
+        
+        self.pr_info = self.fetch_pr_info()
 
         self.pr_title = self.pr_info.get('title', '')
         self.pr_description = self.pr_info.get('body', '')
+        self.pr_diff = self.fetch_pr_patch()
         
-        print(self.pr_title)
-        print(self.pr_description)
-        
-    def fetch_pr_info(self, entry):
-        """Fetch PR information as JSON from the GitHub API URL constructed using entry."""
-        url = f"https://api.github.com/repos/{entry['Owner']}/{entry['Repository']}/pulls/{entry['PullRequestId']}"
-        response = requests.get(url, headers={'Accept': 'application/vnd.github.v3+json'})
+    def fetch_pr_info(self):
+        response = requests.get(self.api_url, headers={'Accept': 'application/vnd.github.v3+json'})
         if response.ok:
             return response.json()
+        return {}
+    
+    def fetch_pr_patch(self):
+        response = requests.get(self.api_url, headers={'Accept': 'application/vnd.github.patch'})
+        if response.ok:
+            return response.text
         return {}
