28fc17465b0a1261e59b35a0b8197f82abdd1342
Rasmus Lindahl <rasmus.lindahl@se.abb.com>
2024-03-17 22:27:13 +0100

    Remove unused unnecessary files

--- pull_request_benchmark/models/evaluation_sample.py
+++ pull_request_benchmark/models/evaluation_sample.py
@@ -1,11 +1,18 @@
-from .pull_request import PullRequest
-from .repository import Repository
-
 class EvaluationSample:
     def __init__(self, pr_url):
-        self.pull_request = PullRequest(pr_url)
-        self.repository = Repository(self.pull_request.repo_url)
+        self.t = 1
         
-    def get_data_point_context_size(self):
-        # To be implemented
-        pass
+    def get_pr_type():
+        return "lol"
+    
+    def get_pr_title():
+        return "lol"    
+    
+    def get_pr_description():
+        return "lol"  
+    
+    def get_pr_diff():
+        return "lol"    
+        
+    def get_pr_outcome():
+        return "rejected"
